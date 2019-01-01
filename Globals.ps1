3062b3bafe4ac955a9015c32b723a42f4b6a994c
kaband <kaband@hotmail.com>
2019-01-01 08:56:39 -0600

    Update

--- Globals.ps1
+++ Globals.ps1
@@ -87,6 +87,78 @@ function Get-ini
 	Return $cfg
 }
 
+# This function made possible by adam the automator - https://www.adamtheautomator.com/powershell-start-process/
+function Invoke-Process
+{
+	<#
+		.VERSION 1.4
+		.GUID b787dc5d-8d11-45e9-aeef-5cf3a1f690de
+		.AUTHOR Adam Bertram
+		.COMPANYNAME Adam the Automator, LLC
+		.TAGS Processes
+	#>
+	[CmdletBinding(SupportsShouldProcess)]
+	param
+	(
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[string]$FilePath,
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[string]$ArgumentList
+	)
+	
+	$ErrorActionPreference = 'Stop'
+	
+	try
+	{
+		$stdOutTempFile = "$env:TEMP\$((New-Guid).Guid)"
+		$stdErrTempFile = "$env:TEMP\$((New-Guid).Guid)"
+		
+		$startProcessParams = @{
+			FilePath			   = $FilePath
+			ArgumentList		   = $ArgumentList
+			RedirectStandardError  = $stdErrTempFile
+			RedirectStandardOutput = $stdOutTempFile
+			Wait				   = $true;
+			PassThru			   = $true;
+			NoNewWindow		       = $true;
+		}
+		if ($PSCmdlet.ShouldProcess("Process [$($FilePath)]", "Run with args: [$($ArgumentList)]"))
+		{
+			$cmd = Start-Process @startProcessParams
+			$cmdOutput = Get-Content -Path $stdOutTempFile -Raw
+			$cmdError = Get-Content -Path $stdErrTempFile -Raw
+			if ($cmd.ExitCode -ne 0)
+			{
+				if ($cmdError)
+				{
+					throw $cmdError.Trim()
+				}
+				if ($cmdOutput)
+				{
+					throw $cmdOutput.Trim()
+				}
+			}
+			else
+			{
+				if ([string]::IsNullOrEmpty($cmdOutput) -eq $false)
+				{
+					Write-Output -InputObject $cmdOutput
+				}
+			}
+		}
+	}
+	catch
+	{
+		$PSCmdlet.ThrowTerminatingError($_)
+	}
+	finally
+	{
+		Remove-Item -Path $stdOutTempFile, $stdErrTempFile -Force -ErrorAction Ignore
+	}
+}
+
 [string]$mydir = Get-MyDir
 [string]$mamepath = $null
 [string]$rompath = $null
